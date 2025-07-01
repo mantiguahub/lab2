d3bb856bd75fbe2b0a867adbafc4f17d7339db04
mantiguahub <mantigua@montgomerycollege.edu>
2025-06-30 21:02:04 -0400

    Create fxdriver.java

---
+++ fxdriver.java
@@ -0,0 +1,21 @@
+//Name: Michael Antigua
+//cmsc 203
+//description:
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FXDriver extends Application {
+
+    public static void main(String[] args) {
+        launch(args);  // required for JavaFX apps
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMainPane root = new FXMainPane();  
+        stage.setScene(new Scene(root, 400, 200));  
+        stage.setTitle("FX GUI Lab");  
+        stage.show();  
+    }
+}
