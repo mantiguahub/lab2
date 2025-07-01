ce88ff8781661afb5cfb7e0e0875ebc53a5eabf0
mantiguahub <mantigua@montgomerycollege.edu>
2025-06-30 21:07:59 -0400

    Create datamanager.java

---
+++ datamanager.java
@@ -0,0 +1,17 @@
+//Name:Michael Antigua
+//cmsc 203
+// description: data manager
+
+public class DataManager {
+    public String getHello() {
+        return "Hello";
+    }
+
+    public String getHowdy() {
+        return "Howdy";
+    }
+
+    public String getJapanese() {
+        return "konnichiwa";
+    }
+}
